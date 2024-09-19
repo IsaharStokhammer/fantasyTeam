f51bfa10d2ceb3e9f1bc455df5931884fc200b8b
IsaharStokhammer <8526656@gmail.com>
2024-09-19 15:25:06 +0300

    a

--- script.ts
+++ script.ts
@@ -6,38 +6,112 @@ const centerContainer = document.getElementById("centerContainer");
 
 const playerTableBody = document.getElementById("playerTableBody");
 
+const searchFrom = document.getElementById("searchFrom");
+searchFrom!.addEventListener("submit",(e:Event)=>{
+    e.preventDefault();
+})
 const positionSearch = document.getElementById("positionSearch") as HTMLFormElement;
 const pointsSearch = document.getElementById("pointsSearch") as HTMLFormElement;
 const threeSearch = document.getElementById("3Search") as HTMLFormElement;
 const twoSearch = document.getElementById("2Search") as HTMLFormElement;
 
-const searchButton = document.getElementById("searchButton");
+const searchButton = document.getElementById("searchButton") as HTMLButtonElement;
+searchButton!.addEventListener('click', () => reloadTable());
 
+
+const playersArr : player[] = [];
 interface player {
     position: string,
     twoPercent: number,
     threePercent: number,
-    points: number
+    points: number,
+    playerName: string
 }
 
-const request = {
-    position: positionSearch.value,
-    twoPercent: +twoSearch.value,
-    threePercent: +threeSearch.value,
-    points: +pointsSearch.value,
+// const request = {
+//     position: positionSearch.value,
+//     twoPercent: +twoSearch.value,
+//     threePercent: +threeSearch.value,
+//     points: +pointsSearch.value,
+// }
+async function reloadTable() {
+    debugger;
+    playerTableBody!.innerHTML = "";
+    await getPlayersToPlayersArr({
+        position: positionSearch.value,
+        twoPercent: +twoSearch.value,
+        threePercent: +threeSearch.value,
+        points: +pointsSearch.value,
+    });
+    playersArr.forEach((currentPlayer : player)=> {
+        const newTr = document.createElement("tr")
+        const nameTd = document.createElement("td");
+        nameTd.innerText = currentPlayer.playerName;
+        const positionTd = document.createElement("td");
+        positionTd.innerText = currentPlayer.position;
+        const pointsTd = document.createElement("td");
+        pointsTd.innerText = currentPlayer.points.toString();
+        const threePercentTd = document.createElement("td");
+        threePercentTd.innerText = currentPlayer.threePercent.toString();
+        const twoPercentTd = document.createElement("td");
+        twoPercentTd.innerText = currentPlayer.twoPercent.toString();
+
+        const addPlayerBtn = document.createElement("button");
+        addPlayerBtn.innerText = `add player ${currentPlayer.playerName} to current team`;
+        addPlayerBtn.addEventListener('click', () => {
+            addPlayer(currentPlayer);
+        });
+
+        newTr.appendChild(nameTd);
+        newTr.appendChild(positionTd);
+        newTr.appendChild(pointsTd);
+        newTr.appendChild(threePercentTd);
+        newTr.appendChild(twoPercentTd);
+        newTr.appendChild(addPlayerBtn);
+        playerTableBody?.appendChild(newTr);
+
+        
+    })
+    
 }
-console.log(request)
 
-fetch('https://nbaserver-q21u.onrender.com/api/filter', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(request)
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+async function getPlayersToPlayersArr(request : object) {   
 
+    fetch('https://nbaserver-q21u.onrender.com/api/filter', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request)
+    })
+    .then(response => response.json())
+    .then(data => playersArr.push(...data))
+    .then(() => console.log(`המידע נכנס למערך שחקנים`))
+    .catch(error => console.error('Error:', error));
 
-  
+}
+
+function addPlayer(currentPlayer: player){
+    const playerElement = document.createElement("div");
+    playerElement.textContent = `point Guard \n ${currentPlayer.playerName} - ${currentPlayer.position}\n ${currentPlayer.points}\n ${currentPlayer.threePercent}%\n ${currentPlayer.twoPercent}%`;
+    switch (currentPlayer.position) {
+        case "PG":
+            
+            pointGuardContainer!.innerHTML = '';
+            pointGuardContainer?.prepend(playerElement);
+            break;
+        case "SG":
+            sootingGuardContainer?.prepend(playerElement);
+            break;
+        case "SF":
+            smallForwardContainer?.prepend(playerElement);
+            break;
+        case "PF":
+            powerForwardContainer?.prepend(playerElement);
+            break;
+        case "C":
+            centerContainer?.prepend(playerElement);
+            break;
+    }
+
+}
